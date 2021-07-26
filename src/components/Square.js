ba4e316eb4d0f1f91272a0e19e1570d863a17ac7
CSapien08 <sapiano08@gmail.com>
2021-07-26 13:59:23 -0700

    added grid with question marks

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 
+
 class Square extends Component{
   render(){
     return(
       <>
+        <div id='grid'>{this.props.value}</div>
       </>
     )
   }
